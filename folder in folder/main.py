022ab7109f36c986f7037a7512ed57b056b3f744
faridehebrahymy <96016472+faridehebrahymy@users.noreply.github.com>
2021-12-20 19:26:18 +0330

    Update main.py

--- folder in folder/main.py
+++ folder in folder/main.py
@@ -1,23 +1,29 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from time import sleep
-driver = webdriver.Firefox("C:\Users\Parseh\datin_project\my_venv\geckodriver.exe")
 
+driver = webdriver.Firefox()
 driver.get("https://www.digikala.com")
 email = "cabelucamelia@gmail.com"
-password = "qwerty997wertR"
+password = "Camelia2323"
+expected="Camelia Cabelo"
 
 def TestLogin():
         driver.find_element_by_xpath("/html/body/header/div/div/div[2]/div[1]/div/a").click()
         sleep(2)
-        email_phone = driver.find_element_by_name('login[email_phone]')    
+        email_phone = driver.find_element_by_name('login[email_phone]')
         email_phone.send_keys(email)
-        driver.find_element_by_xpath("//*[@id="loginForm"]/button").click()
+        driver.find_element_by_xpath('//*[@id="loginForm"]/button').click()
         sleep(3)
-        driver.find_element_by_name('login[password]').send_keys(password)
-        driver.find_element_by_xpath("//*[@id="authForm"]/button").click()
+        driver.find_element_by_name("login[password]").send_keys(password)
+        driver.find_element_by_xpath('//*[@id="authForm"]/button').click()
         sleep(3)
         driver.find_element_by_xpath("/html/body/header/div/div/div[2]/div[1]/div/a").click()
+        driver.find_element_by_xpath("/html/body/header/div/div/div[2]/div[1]/div/div/div[1]/a").click()
+        actual =driver.find_element_by_xpath("/html/body/main/div[2]/div/section/div[1]/section[1]/div/div/div[1]/div/div[2]/div[1]")
+        assert actual.text==expected
 
 
 TestLogin()
